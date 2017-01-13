8186177d255fba6d6f507369246ea18e80299384
ded1ad <bigvasya97@mail.ru>
2017-01-13 09:54:26 +0300

    Add files via upload

---
+++ 8.4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char* str = ". .mn . .", res[256], *p = res, *w = " ", *bw = "!";
+	size_t bwLen = strlen(bw), wLen = strlen(w);
+	for (; *p = *str; ++str, ++p)
+	{
+		if (!strncmp(w, str, wLen))
+		{
+			strcpy(p, bw);
+			p += bwLen - 1;
+			str += wLen - 1;
+		}
+	}
+	puts(res);
+	return 0;
+}
